572205c40884a2e2bef0abea7e7460912682e0c3
Kouassi15 <rachelleservices8@gmail.com>
2023-02-13 15:11:01 +0100

    first commit

---
+++ Exercise4/test/Exampletest.php
@@ -0,0 +1,14 @@
+use PHPUnit\Framework\TestCase;
+
+    class ExampleTest extends TestCase
+    {
+        // Tests will go here
+
+        public function testClass()
+            {
+                $user = new ExampleTest(18, 'John');
+            }
+    }
+
+    
+?>
